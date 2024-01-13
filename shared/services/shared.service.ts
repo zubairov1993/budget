da34168df562cefd9967cb6a522f50ea6e013cfa
Vadim <vadim.zubairov1993@gmail.com>
2024-01-13 14:45:20 +0600

    'fix'

--- shared/services/shared.service.ts
+++ shared/services/shared.service.ts
@@ -110,11 +110,9 @@ export class SharedService {
 
         const currentDate: Date = new Date()
         const currentYear: number = currentDate.getFullYear()
-
         if (year === currentYear) {
           this.currentYearUid = id
           this.monthlyBudget$.next(data[id].monthlyBudget.monthlyBudget)
-
         }
 
         if (monthsData) {
